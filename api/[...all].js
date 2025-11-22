eb7bb011cfd04ad12e807e1207c0c7ef9e92d236
dinfluencer <digitalinfluenceradm@gmail.com>
2025-11-22 11:26:17 -0300

    fix(cors): include PUT in allowed methods across layers

--- api/[...all].js
+++ api/[...all].js
@@ -2,7 +2,7 @@ import app from '../app.js'
 export default function handler(req,res){
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*')
   res.setHeader('Vary', 'Origin')
-  res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE,OPTIONS')
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE,PUT,OPTIONS')
   res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization, X-Requested-With, Accept, Origin')
   res.setHeader('Access-Control-Max-Age','600')
   if(req.method==='OPTIONS'){res.status(204).end();return}
